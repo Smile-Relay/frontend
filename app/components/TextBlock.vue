d01bc789c77cbf0e2e085226bb3d76b10a87b090
huangdihd <hd20100104@163.com>
2026-01-20 19:06:20 +0800

    Increase text size in text block component

--- app/components/TextBlock.vue
+++ app/components/TextBlock.vue
@@ -15,7 +15,7 @@ defineExpose<{
 </script>
 <template>
   <div class="max-w-3xl mx-0 rounded-2xl border border-gray-200 bg-white p-6 shadow-sm">
-    <p class="leading-relaxed text-gray-700">
+    <p class="text-4xl leading-relaxed text-gray-700">
       {{ props.text }}
     </p>
   </div>
