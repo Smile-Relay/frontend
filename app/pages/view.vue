18be52a6783065cee02776bbfb80dddc4093ec22
huangdihd <hd20100104@163.com>
2026-01-21 21:22:40 +0800

    Refactor view page buttons to use UButton component

--- app/pages/view.vue
+++ app/pages/view.vue
@@ -88,8 +88,8 @@ const handleAction = async (action: string) => {
     </div>
 
     <div v-show="id" class="mt-6 flex gap-4">
-      <button class="px-6 py-2 bg-gray-500 text-white rounded-lg text-2xl" @click="router.push('/')">离开</button>
-      <button class="px-4 py-2 bg-blue-500 text-white rounded-lg text-2xl" @click="get_new">换一个</button>
+      <UButton class="bg-gray-500 hover:bg-gray-400 text-white text-2xl" @click="router.push('/')">离开</UButton>
+      <UButton class="bg-blue-500 hover:bg-blue-400 text-white text-2xl" @click="get_new">换一个</UButton>
     </div>
     <div v-show="!id">
       <p class="text-3xl text-amber-50">暂时没有已扔出的漂流瓶</p>
