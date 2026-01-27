8c3b5d7849d3dc834c8fc9684dadd3c15ebcf0bf
huangdihd <hd20100104@163.com>
2026-01-27 19:06:43 +0800

    Updated HexChar type to include uppercase letters A-F

--- app/composables/usePhrase.ts
+++ app/composables/usePhrase.ts
@@ -5,8 +5,8 @@ type Phrase = `${typeof vocabularies.adjectives[number]}${typeof vocabularies.no
 type HexChar =
     | "0" | "1" | "2" | "3"
     | "4" | "5" | "6" | "7"
-    | "8" | "9" | "a" | "b"
-    | "c" | "d" | "e" | "f" ;
+    | "8" | "9" | "A" | "B"
+    | "C" | "D" | "E" | "F" ;
 
 type ID = `${HexChar}${HexChar}`;
 
